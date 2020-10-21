a493a3bf232d70a8a915e3a26a7d0de673c108ca
Alexander Kaufman <alexanderkaufman@montana.edu>
2020-10-21 12:18:03 -0600

    psd - documentation improvements
    added potential inputs for the wavepy_og function based on old WavePy3.

--- wavepy3/atmos/psd.py
+++ wavepy3/atmos/psd.py
@@ -1,11 +1,9 @@
 import numpy as np
 
-'''
-These psd's return functions of frequency with the given parameters
-'''
 
 def modified_vonkarman(L0, l0):
-    ''' vonKarman
+    ''' modified_vonKarman
+    - return psd function of r0 and f
 
     This is the modified von Karman PSD function.
     The von Karman PSD is equivalent to fm = 'inf'.
@@ -34,12 +32,31 @@ def modified_vonkarman(L0, l0):
 
 
 def vonkarman(L0):
+    '''
+    vonKarman psd
+    - return psd function of r0 and f
+
+    wrapper for modified_vonKarman w/ l0=0
+    '''
     return modified_vonkarman(L0, l0=0)
 
 
 def kolmogorov():
+    '''
+    kolmogorov psdfn
+    - return psd function of r0 and f
+
+    wrapper for modified_vonKarman w/ L0 = inf and l0 = 0
+    '''
     return modified_vonkarman(L0=float('inf'), l0=0)
 
 # I do not know the proper name of this psd
-def wavepy_og():
-    pass
+def wavepy_og(L0, l0, c_one, theta, aniso, c, alpha):
+    '''
+    The psd implemented in the original wavepy
+    I do not know it's name or if it has one.
+    Based on the original wavepy.validate() routine in WavePy
+    this psd is able to generate screens with average structure
+    function close to theory
+    '''
+    pass 
