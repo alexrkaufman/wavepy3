3cc80e9293f4bb01b172679c50cca524b341425c
Alexander Kaufman <alexanderkaufman@montana.edu>
2020-10-22 11:59:22 -0600

    rearanging vac example and adding phase to verfication graphs

--- vac_example.py
+++ vac_example.py
@@ -9,16 +9,17 @@ def main():
     D_0 = 2e-3
     D_n = 6e-3
     wvl = 1e-6
-    N = 128
-    n_prop = 5
-    z = 2 * np.linspace(0, 1, n_prop + 1)
+    z = 2
     dx_0 = D_0 / 30
     dx_n = D_n / 30
+    N = 128
+    n_prop = 5
+    z = z * np.linspace(0, 1, n_prop + 1)
 
     x = np.linspace(-N * dx_0/2, N * dx_0/2, N)
     x, y = np.meshgrid(x, x)
 
-    field_in = rect(x / (30 * dx_0)) * rect(y / (30 * dx_0))
+    field_in = rect(x / D_0) * rect(y / D_0)
     field_out = wp.propagate(field_in, wvl, dx_0, dx_n, z)
 
     I_in = np.absolute(field_in)**2
@@ -33,11 +34,13 @@ def main():
     ax[1].imshow(I_out)
     plt.show()
 
-    #plt.plot(x[64, :], I_analytic)
+    fig, ax = plt.subplots(1, 2)
     sim_out = I_out[:, N // 2]
     analytic_out = I_analytic
-    plt.plot(x[N // 2, :], sim_out / np.amax(sim_out))
-    plt.plot(x[N // 2, :], analytic_out / np.amax(analytic_out))
+    ax[0].plot(x[N // 2, :], sim_out / np.amax(sim_out))
+    ax[0].plot(x[N // 2, :], analytic_out / np.amax(analytic_out))
+    ax[1].plot(x[N // 2, :], np.angle(field_out[:, N // 2]))
+    ax[1].plot(x[N // 2, :], np.angle(field_analytic))
     plt.show()
 
 
